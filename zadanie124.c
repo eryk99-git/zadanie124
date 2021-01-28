0eea4e90d7723635daed95f30c2de97a220b15b1
Eryk Ł <eryk.lechtanski81@gmail.com>
2021-01-28 20:47:30 +0100

    pole1

---
+++ zadanie124.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+float pole_prostokata(float a, float b);
+float pole_trojkata(float a, float b, float c);
+
+int main() {
+
+    float dl_boku_1, dl_boku_2, dl_boku_3, pole_pros, pole_troj;
+
+    printf("Podaj dlugosc pierwszego boku: ");
+    scanf ("%f", &dl_boku_1);
+
+    printf("\nPodaj dlugosc drugiego boku: ");
+    scanf ("%f", &dl_boku_2);
+
+    printf("\nPodaj dlugosc trzeciego boku: ");
+    scanf ("%f", &dl_boku_3);
+
+    pole_pros = pole_prostokata(dl_boku_1, dl_boku_2);
+    printf ("\nPole prostokata wynosi: %.2f", pole_pros);
+    printf("\n");
+
+	pole_troj = pole_trojkata(dl_boku_1, dl_boku_2, dl_boku_3);
+    printf ("\nPole trojkata wynosi: %.2f", pole_troj);
+    printf("\n");
+
+return 0;
+    }
